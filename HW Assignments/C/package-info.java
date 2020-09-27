c0c4aafb7027702ba948c6f6288dc0b079196122
Gavin <gavinmwong@gmail.com>
2020-09-27 15:27:45 -0700

    added Homework Assignments

---
+++ HW Assignments/C/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ *  TODO Write a one-sentence summary of your class here.
+ *  TODO Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author  gwong676
+ *  @version Mar 16, 2018
+ *  @author  Period: TODO
+ *  @author  Assignment: HW Assignments
+ *
+ *  @author  Sources: TODO
+ */
+package C;
